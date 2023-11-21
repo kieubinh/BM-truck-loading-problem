68233b351af5099520796cbfdbb4b95410e67b8d
Kieu Thanh Binh (Anthony Kieu) <binhkt.vnu@gmail.com>
2023-11-21 12:10:26 +0000

    add input file

--- src/arrangement.py
+++ src/arrangement.py
@@ -1,1 +1,13 @@
+import pandas as pd
 
+truck_df = pd.read_csv('data/trucks.csv')
+item_df = pd.read_csv('data/items.csv')
+input_df = pd.read_csv('data/input.csv')
+
+## check data
+print(truck_df)
+print(item_df)
+print(input_df)
+
+## optimization
+total_weights = 
